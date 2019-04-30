17c20d5486d98e4975c690702aa89f8bd3add916
walterlee <walter.lee9@gmail.com>
2019-04-30 15:38:52 -0500

    declarative rendering

---
+++ vue.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+
+var app2 = new Vue({
+    el: '#app2',
+    data: {
+        message: 'Hello World!'
+    }
+})
