f799d0eb007d2ef24a5c120fe20fe88630498a5c
walterlee <walter.lee9@gmail.com>
2019-05-08 14:04:51 -0500

    css update

--- build/vue.js
+++ build/vue.js
@@ -16,6 +16,7 @@ var app2 = new Vue({
 var app3 = new Vue({
     el: '#app-2',
     data: {
+        title: "Below text uses the v-bind directive, which applies reactive behavior to the rendered DOM",
         message: 'You loaded this page on ' + new Date().toLocaleString()
     }
 })
