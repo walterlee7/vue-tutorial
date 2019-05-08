3acae4527a5cca48fe5f4d06ba5adb23e4ea7003
walterlee <walter.lee9@gmail.com>
2019-05-08 17:49:44 -0500

    using v-if and listing objects

--- build/vue.js
+++ build/vue.js
@@ -16,7 +16,26 @@ var app2 = new Vue({
 var app3 = new Vue({
     el: '#app-2',
     data: {
-        title: "Below text uses the v-bind directive, which applies reactive behavior to the rendered DOM",
+        title: "Below text uses the v-bind directive, which applies reactive behavior to the rendered DOM.",
         message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+})
+
+var app4 = new Vue({
+    el: '#app-3',
+    data: {
+        title: "Below text uses v-if to show text based on a boolean.",
+        seen: true
+    }
+})
+
+var app5 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+        ]
     }
 })
