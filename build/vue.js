7490f74145fa9d631ce26d5acd4de7154873b2ac
walterlee <walter.lee9@gmail.com>
2019-05-08 18:15:46 -0500

    added v-on example

--- build/vue.js
+++ build/vue.js
@@ -37,5 +37,27 @@ var app5 = new Vue({
             { text: 'Learn Vue' },
             { text: 'Build something awesome' }
         ]
+    },
+    methods: {
+
+        reverseText: function () {
+            this.todos[0].text = this.todos[0].text.split('').reverse().join('')
+        },
+
+        addItem: function () {
+            app5.todos.push({ text: 'New item' })
+        }
+    }
+})
+
+var app6 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
     }
 })
